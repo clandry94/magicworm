06a8ffb0ac203cd391d814507b3f0b8066727f9d
Conor Landry <clandry94@ufl.edu>
2014-11-20 19:55:22 -0500

    Changed from position to artifact since position was too descriptive

---
+++ artifact/artifact.h
@@ -0,0 +1,9 @@
+struct artifact {
+
+  position(int x, int y, int type, string color);
+  int x;
+  int y;
+  int type; //1 = snake, 2 = food, 3 = powerup
+  string color;
+
+};
--- position/position.h
+++
@@ -1,8 +0,0 @@
-struct position {
-
-  position(int x, int y, int type);
-  int x;
-  int y;
-  int type; //1 = snake, 2 = food
-
-};
