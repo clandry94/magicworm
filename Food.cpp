5b77a4476fb96a97e80aae3dfc5e9947fdb5cb22
Keanu <keanukerr@gmail.com>
2014-11-20 19:23:26 -0500

    Create Food.cpp
    Outline for food.cpp

---
+++ Food.cpp
@@ -0,0 +1,14 @@
+#include "position.h"
+#include "food.h"
+
+using namespace std;
+
+// Generate random X and Y coordinates for food to appear on
+void generateFood() {
+  
+}
+
+// Draw food on Gameboard
+void drawFood() {
+
+}
