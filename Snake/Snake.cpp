2c1a083074aa9721292dc4043eb37364307ef156
Conor Landry <clandry94@ufl.edu>
2014-11-20 19:35:58 -0500

    Boilerplate for snake class v2

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
+#include "../position/position.h"
 #include "snake.h"
 
-Snake::Snake(int size, int speed) {
-
+Snake::Snake(int size, int speed, string color, position startCoords) {
+  size = this->size;
+  speed = this->speed;
+  color = this->color;
+  coordinates.at(0).x = startCoords.x;
+  coordinates.at(0).y = startCoords.y;
+  coordinates.at(0).type = 1;
 }
 
 int Snake::getSize() {
   return size;
 }
 
-void Snake::setSize(int val) {
-  size += val;
+void Snake::incrementSize() {
+  size += 1;
 }
+
 
 int Snake::getSpeed() {
   return speed;
 }
 
 void Snake::setSpeed(int val) {
-  speed += val;
+  speed = val;
+}
+
+void Snake::incrementSpeed() {
+  speed += 1;
+}
+
+void Snake::eat() {
+  incrementSize();
+  incrementSpeed();
+}
+
+vector<position> getPositions() {
+  return coordinates;
+}
+
+void setPositions(int position, int x, int y) {
+  coordinates.at(position).x = x;
+  coordinates.at(position).y = y;
 }
--- Snake/Snake.h
+++ Snake/Snake.h
@@ -1,21 +1,37 @@
+#include <vector>
+
 #ifndef SNAKE_H
 #define SNAKE_H
+
+using namespace std;
 
 class Snake {
 
 	private:
 		int size;	//size of the snake
 		int speed;	//speed of the snake
+		string color;
+		vector<position> coordinates;
 
 	public:
-		Snake(int size, int speed);
-		
+		bool hasPowerup; //Snake has powerup t/f
+
+		Snake(int size, int speed, string color, position initCoords);
+
 		int getSize();
-		void setSize(int val);
+		void incrementSize();
 
 		int getSpeed();
 		void setSpeed(int val);
+		void incrementSpeed();
 
+		string getColor();
+		void setColor(string newColor);
+
+		void eat();
+
+		vector<position> getPositions();
+		void setPositions();
 };
 
 
