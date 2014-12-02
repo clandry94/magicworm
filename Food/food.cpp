6070d7fe9fd30a2f4aaf7557ce7508cd4b59a2d5
Keanu Kerr <keanukerr@gmail.com>
2014-12-02 16:18:21 -0500

    Food
    minor changes

--- Food/food.cpp
+++ Food/food.cpp
@@ -1,14 +1,31 @@
-#include "position.h"
+#include "../artifact/artifact.h"
+#include "../gameboard/gameboard.h"
+#include "../food/food.h"
+#include "../snake/snake.h"
+#include "../powerup/powerup.h"
 #include "food.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <GL/glew.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
 
 using namespace std;
 
 // Generate random X and Y coordinates for food to appear on
-void generateFood() {
-  
-}
+void Food::generateFood() {
+  // When the snake intersects with the food or score is 0 (for when the game starts)
+  if ((snakeX == foodX && snakeY == foodY) || (score = 0)){
+    // Generate new food location
+    foodX = rand() % 100 + 1;
+    foodY = rand() % 100 + 1;
 
-// Draw food on Gameboard
-void drawFood() {
+    // Draw food
 
+
+  }
 }
--- Food/food.h
+++ Food/food.h
@@ -1,1 +1,17 @@
 // Food header file
+
+#ifndef FOOD_H
+#define FOOD_H
+
+using namespace std;
+
+class Food {
+
+  private:
+    int foodX;
+    int foodY;
+    void generateFood();
+
+};
+
+#endif
