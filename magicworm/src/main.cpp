3379d2b7594dbcd2133d49b0581091d5923c6fe6
Conor Landry <clandry94@ufl.edu>
2014-12-03 20:30:19 -0500

    Added basic res pictures and basic SDL implementation

--- magicworm/src/main.cpp
+++ magicworm/src/main.cpp
@@ -1,12 +1,113 @@
 #include "Snake/Snake.cpp"
 #include <SDL2/SDL.h>
+
 using namespace std;
 
-int main() {
-  if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+
+/*
+  Logs error created from the SDL graphics
+*/
+void logSDLError(ostream &os, const string &msg) {
+  os << msg << " error: " << SDL_GetError() << endl;
+}
+
+SDL_Texture * loadTexture(const string &file, SDL_Renderer *ren) {
+  //init to nullptr for no pointer crashes
+  SDL_Texture *texture = nullptr;
+
+  //Load background image
+  SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+
+  //If the loading went ok, convert to texture and return
+  if(loadedImage != nullptr) {
+    texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		//Make sure converting went ok
+		if (texture == nullptr){
+			logSDLError(std::cout, "CreateTextureFromSurface");
+		}
+	} else {
+    logSDLError(cout, "LoadBMP");
   }
+
+  return texture;
+}
+
+/*
+  * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+  * the texture's width and height
+  * @param tex The source texture we want to draw
+  * @param ren The renderer we want to draw to
+  * @param x The x coordinate to draw to
+  * @param y The y coordinate to draw to
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+	//Setup the destination rectangle to be at the position we want
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	//Query the texture to get its width and height to use
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+
+int main() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+  	logSDLError(std::cout, "SDL_Init");
+  	return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("MagicWorm", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == nullptr){
+  	logSDLError(std::cout, "CreateWindow");
+  	SDL_Quit();
+  	return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  if (renderer == nullptr){
+  	logSDLError(std::cout, "CreateRenderer");
+  	//cleanup(window);
+  	SDL_Quit();
+  	return 1;
+  }
+
+  const string backgroundPath = "/Users/landry/Documents/cop3503/magicworm/res/magicworm/background.bmp";
+  const string snakePath = "/Users/landry/Documents/cop3503/magicworm/res/magicworm/square.bmp";
+  SDL_Texture *background = loadTexture(backgroundPath, renderer);
+  SDL_Texture *image = loadTexture(snakePath, renderer);
+  if (background == nullptr || image == nullptr){
+  	//cleanup(background, image, render, window);
+  	SDL_Quit();
+  	return 1;
+  }
+
+  SDL_RenderClear(renderer);
+
+  int bW, bH;
+  SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+  renderTexture(background, renderer, 0, 0);
+  renderTexture(background, renderer, bW, 0);
+  renderTexture(background, renderer, 0, bH);
+  renderTexture(background, renderer, bW, bH);
+
+  int iW, iH;
+  SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+  int x = SCREEN_WIDTH / 2 - iW / 2;
+  int y = SCREEN_HEIGHT / 2 - iH / 2;
+  renderTexture(image, renderer, x, y);
+
+  SDL_RenderPresent(renderer);
+  SDL_Delay(5000);
+  //SDL_DestroyTexture(image);
+  //SDL_DestroyRenderer(renderer);
+  SDL_Delay(5000);
+
+  //cleanup(background, image, render, window);
   SDL_Quit();
-  return 0;
 }
